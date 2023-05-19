325c5cfec2323329b7a35422178ed0be2f5ae71b
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-05-19 15:42:27 -0700

    working it

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # stackblitz-webcontainer-api-starter-8bh3eh
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-webcontainer-api-starter-8bh3eh)
+
+# Running from localhost? 
+- python -m http.server 3030 
+- http://localhost:3030/
--- memory.js
+++ memory.js
@@ -6,10 +6,30 @@ function launchModal() {
 function closeModal() {
     modal.style.display = "none";
 }
+///////////// FROM CHILD TO PARENT 
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
+window.addEventListener("message", gotMessageFromIFrame, false);
+function gotMessageFromIFrame(event) {
+  const msg = "gotMessageFromIFrame:" +  event.data
+  console.log("%c " + msg , "background:lightyellow");
+}
+////////////// FROM PARENT TO CHILD 
+const iframe = document.getElementById("modalContent");
+function sendMessage() {
+    try {
+        console.log("%c sendMessage memory.js ", "background:lightyellow")
+        const r = Math.random() 
+        const obj = {
+            r:r, 
+            "hello":"there"
+        }
+        iframe.contentWindow.postMessage(obj, "*");
+    } catch (boom) {
+        console.error(boom)
+    }
+}
 
